8803734ddfaa041ab9ad4935162ff29aae50c6f6
hong4cong <hong4cong@gmail.com>
2016-10-09 18:52:05 +0800

    update

---
+++ testRouter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testRouter
+//
+//  Created by 洪聪 on 16/10/9.
+//  Copyright © 2016年 lanxiniu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testRouter/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  testRouter
+//
+//  Created by 洪聪 on 16/10/9.
+//  Copyright © 2016年 lanxiniu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
